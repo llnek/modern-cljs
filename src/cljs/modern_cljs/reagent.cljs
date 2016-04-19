286424f61e6af73387d0200cf7cf932d6435e528
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2016-04-19 19:38:08 +0200

    marked

--- src/cljs/modern_cljs/reagent.cljs
+++ src/cljs/modern_cljs/reagent.cljs
@@ -1,24 +1,40 @@
 (ns modern-cljs.reagent
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            cljsjs.marked))
 
-(defn comment-component [author comment]
+(def data (r/atom [{:id 1
+                    :author "Pete Hunt"
+                    :text "This is one comment"}
+                   {:id 2
+                    :author "Jordan Walke"
+                    :text "This is *another* comment"}]))
+
+(defn comment-component 
+  [author comment]
   [:div 
    [:h2 author]
-   comment])
+   [:span {:dangerouslySetInnerHTML 
+           #js {:__html (js/marked comment)}}]])
 
-(defn comment-list []
+(defn comment-list [comments]
   [:div  
-   [comment-component "Pete Hunt" "This is one comment"] 
-   [comment-component "Jordan Walke" [:p "This is " [:em "another"] " comment"]]])
+   (for [{:keys [id author text]} @comments] 
+     ^{:key id} [comment-component author text])])
 
-(defn comment-form []
-  [:div "Hello, world! I am a comment-form"])
+(defn comment-form [data]
+  [:form
+        [:input {:type "text"
+                 :placeholder "Your name"}]
+        [:input {:type "text"
+                 :placeholder "Say something"}]
+        [:input {:type "button" :value "Post"
+                 :on-click #(.log js/console "Posted")}]])
 
 (defn comment-box []
   [:div 
    [:h1 "Comments"]
-   [comment-list]
-   [comment-form]])
+   [comment-list data]
+   [comment-form data]])
 
 (defn ^:export init []
   (if (and js/document
